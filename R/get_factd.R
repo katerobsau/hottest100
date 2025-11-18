316b79c6c657c381825c405ac7a039ce852856d0
Kate Saunders <kate.saunders@monash.edu>
2025-11-18 17:26:23 +1100

    Package

---
+++ R/get_factd.R
@@ -0,0 +1,3 @@
+get_factd <- function(){
+  print("What to write something that returns fun facts about the hottest 100")
+}
---
+++ R/who_is_the_hottest.R
@@ -0,0 +1,3 @@
+who_is_the_hottest <- function(){
+  print("Want to write a silly function that returns the hottest by filter call")
+}

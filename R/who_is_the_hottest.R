c2fc55db8d9037aa5f1992e6047a1c53ca15f26c
Kate Saunders <kate.saunders@monash.edu>
2025-11-19 18:28:37 +1100

    Error handling of inputs added

--- R/who_is_the_hottest.R
+++ R/who_is_the_hottest.R
@@ -1,18 +1,29 @@
 who_is_the_hottest <- function(year, special = NULL){
 
+  year_range = hottest100_tracks$pollyear |> range()
+
+  if(year < min(year_range) | year > max(year_range) |
+     (year == 1989 & is.null(special))){
+    stop("Sorry that countdown isn't in this dataset")
+  }
+
   if(is.null(special)){
 
     countdown_winner = hottest100_tracks |>
       filter(pollyear == year & position == 1 & alltime == FALSE)
 
-    glue("In {year}, {countdown_winner$artist} was the hottest!") |> print()
+    output_str = glue("In {year}, {countdown_winner$artist} was the hottest!")
+
+    print(output_str)
 
   }else{
 
     countdown_winner = hottest100_tracks |>
       filter(pollyear == year & position == 1 & alltime == TRUE)
 
-    glue("For the special countdown in {year} - {countdown_winner$artist} was the hottest!") |> print()
+    output_str = glue("For the special countdown in {year} - {countdown_winner$artist} was the hottest!")
+
+    print(output_str)
 
   }
 

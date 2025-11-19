027f6b80757baac7aae760cc3d1e0d6eace8287d
Dianne Cook <visnut@gmail.com>
2025-11-20 06:33:07 +1100

    documented data, tidied up DESCRIPTION, shortened data names

---
+++ R/data.R
@@ -0,0 +1,48 @@
+#' Information about albums on the TripleJ Hottest 100
+#'
+#' \itemize{
+#'   \item year
+#'   \item alltime
+#'   \item alltimedescription
+#'   \item colorRGB
+#'   \item contrastColorRGB
+#' }
+#'
+#' @name albums
+#' @docType data
+#' @format A 42 x 5 data frame
+#' @keywords datasets
+NULL
+
+#' Information about artists on the TripleJ Hottest 100
+#'
+#' \itemize{
+#'   \item artist
+#'   \item country
+#'   \item releaseyear
+#' }
+#'
+#' @name artists
+#' @docType data
+#' @format A 2729 x 3 data frame
+#' @keywords datasets
+NULL
+
+#' Information about tracks on the TripleJ Hottest 100
+#'
+#' \itemize{
+#'   \item alltime
+#'.  \item artist
+#'   \item country
+#'   \item id
+#'   \item pollyear
+#'.  \item position
+#'   \item releaseyear
+#'   \item track
+#' }
+#'
+#' @name tracks
+#' @docType data
+#' @format A 4200 x 8 data frame
+#' @keywords datasets
+NULL

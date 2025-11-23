55835ab7643299a24fb4e22517eeead47dd5483b
njtierney <nicholas.tierney@gmail.com>
2025-11-23 11:43:29 +1100

    add pkg::fun(); add missing arguments; import dplyr and glue

--- R/who_is_the_hottest.R
+++ R/who_is_the_hottest.R
@@ -2,6 +2,10 @@
 #'
 #' This function will help searching for information by year
 #'
+#' @param year integer, pollyear to search in `tracks` data.
+#' @param special logical, whether to include `alltime` in the data. Default is
+#'   to not include this.
+#'
 #' @export
 #' @examples
 #' who_is_the_hottest(2023)
@@ -15,19 +19,19 @@ who_is_the_hottest <- function(year, special = NULL){
   if(is.null(special)){
 
     countdown_winner = tracks |>
-      filter(pollyear == year & position == 1 & alltime == FALSE)
+      dplyr::filter(pollyear == year & position == 1 & alltime == FALSE)
 
     if(nrow(countdown_winner) == 0)
       stop("Sorry that countdown isn't in this dataset.")
 
-    output_str = glue("In {year}, {countdown_winner$artist} was the hottest!")
+    output_str = glue::glue("In {year}, {countdown_winner$artist} was the hottest!")
 
   }else{
 
     countdown_winner = tracks |>
-      filter(pollyear == year & position == 1 & alltime == TRUE)
+      dplyr::filter(pollyear == year & position == 1 & alltime == TRUE)
 
-    output_str = glue("For the special countdown in {year} - {countdown_winner$artist} was the hottest!")
+    output_str = glue::glue("For the special countdown in {year} - {countdown_winner$artist} was the hottest!")
 
   }
 
